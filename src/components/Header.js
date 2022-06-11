dff1907f62764a2f28f618d74490b5f257f2fec1
aselwilliams <aselwilliams22@gmail.com>
2022-06-11 08:41:31 -0400

    header is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <h1>World</h1>
+        <img src='https://freefrontend.com/assets/img/css-clocks/analog-clock.gif' alt='clock' />
+        <h1>Clocks</h1>
+    </header>
   )
 }
 
