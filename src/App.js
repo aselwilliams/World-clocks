e5d194412dd0b44fd933538f2eb2958afc68ead5
aselwilliams <aselwilliams22@gmail.com>
2022-06-11 08:28:04 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import moment from 'moment-timezone';
+import {v4 as uuidv4} from 'uuid'
 import './App.css';
 
 function App() {
+  const [timeZones, setTimeZones]=useState(moment.tz.names());
+  const [selectedTZName, setSelectedTZName] =useState('');
+  const [selectedTZTime, setSelectedTZTime] =useState('');
+  const [selectedTZId, setSelectedTZId] =useState('');
+  const [listOfSelectedTZ, setListOfSelectedTZ]= useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
