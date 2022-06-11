9c3fcde51bafa9a5ba83a76607cc53fb8eef7dbe
aselwilliams <aselwilliams22@gmail.com>
2022-06-11 09:14:10 -0400

    functions added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,34 @@ function App() {
   const [selectedTZTime, setSelectedTZTime] =useState('');
   const [selectedTZId, setSelectedTZId] =useState('');
   const [listOfSelectedTZ, setListOfSelectedTZ]= useState([]);
+
+  const handleSelectChange=(e)=>{
+    if(e.target.value!=='- Select a timezone -') {
+      setSelectedTZName(e.target.value);
+      setSelectedTZTime(moment().tz(e.target.value).format('hh:mm:ss A'));
+      selectedTZId(uuidv4())
+    }
+  }
+  const handleAddClock=()=> {
+    const selectedTZIndex=listOfSelectedTZ.findIndex((tz)=>tz.zoneName===selectedTZName);
+
+    if (selectedTZIndex < 0 && selectedTZName !=='') {
+      const newList = listOfSelectedTZ.map((tz)=>({...tz}));
+      const newTZ= {
+        id: selectedTZId,
+        zoneName: selectedTZName,
+        zoneTime: selectedTZTime
+      }
+      newList.push(newTZ);
+      setListOfSelectedTZ(newList)
+    }
+  };
+
+  const handleTZRemove = (id) => {
+    const newList= listOfSelectedTZ.filter((tz)=>tz.id !== id);
+    setListOfSelectedTZ(newList);
+  }
+
   return (
     <div className="App">
     <Header />
