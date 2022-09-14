d9dddfffa55d1fa8619f4cae9e09561e50b8bab0
aselwilliams <aselwilliams22@gmail.com>
2022-09-13 21:12:03 -0500

    changes in style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>World</h1>
+      <h1 className='title'>World</h1>
       <img
         src="https://freefrontend.com/assets/img/css-clocks/analog-clock.gif"
         alt="clock"
       />
-      <h1>Clocks</h1>
+      <h1 className="title">Clocks</h1>
     </header>
   );
 }
